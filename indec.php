ffca923a9c9bf6eec74538481c1ab04f93aaec0d
rmc0035 <rmc0035@mix.wvu.edu>
2021-03-12 14:22:58 -0500

    Lab 2

---
+++ indec.php
@@ -0,0 +1,56 @@
+<main>
+<link rel="stylesheet" href="css/about.css">
+
+<div class="about">
+    <h1>
+        About Us
+    </h1>
+    <p>bgfeuighewusihbgurewishgiurewhguw</p>
+</div>
+<div class="row">
+    <div class="column">
+        <div class="card">
+            <img src="images/1.jpeg">
+            <div class="container">
+                <h2>Main Person</h2>
+                <p>Person</p>
+                <p>Email: hi@fake.address</p>
+            </div>
+        </div>
+        
+    </div>
+    <div class="column">
+    <div class="card">
+            <img src="images/2.jpeg">
+            <div class="container">
+                <h2>Second Person</h2>
+                <p>Person 2</p>
+                <p>Email: hi@fake.address</p>
+            </div>
+        </div>
+        
+    </div>
+    <div class="column">
+    <div class="card">
+            <img src="images/3.jpeg">
+            <div class="container">
+                <h2>Third Person</h2>
+                <p>Person 3</p>
+                <p>Email: hi@fake.address</p>
+            </div>
+        </div>
+        
+    </div>
+    <div class="column">
+    <div class="card">
+            <img src="images/4.jpeg">
+            <div class="container">
+                <h2>Fourth Person</h2>
+                <p>Person 4</p>
+                <p>Email: hi@fake.address</p>
+            </div>
+        </div>
+        
+    </div>
+</div>
+</main>
